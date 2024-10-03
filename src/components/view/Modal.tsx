1b05d4911da1d1db655a066eaa3ad299254c0999
김동균 <wooheuy5752@nate.com>
2024-10-03 17:09:56 +0900

    fix : 모달 구조 수정

--- src/components/view/Modal.tsx
+++ src/components/view/Modal.tsx
@@ -3,12 +3,13 @@
 import { type PropsWithChildren, useRef, useState } from 'react'
 
 import { useScrollLock } from '@/hooks'
+import { zIndex } from '@/utility/constants'
 
 import { Icon } from './icons'
 
 type ModalProps = {
   isOpen: boolean
-  onClose: () => void
+  onClose: VoidFunction
 }
 
 export const Modal = ({ children, isOpen, onClose }: PropsWithChildren<ModalProps>) => {
@@ -28,17 +29,20 @@ export const Modal = ({ children, isOpen, onClose }: PropsWithChildren<ModalProp
 
   return (
     isOpen && (
-      <div
-        className="fixed inset-0 flex items-center justify-center bg-gray-6 mix-blend-multiply"
-        ref={modalBackground}
-        onClick={handleBackgroundClick}
-        aria-hidden="true"
-      >
-        <div className="flex-column relative w-[87.2%]  max-w-[390px] rounded-[12px] bg-white pb-[24px] pt-[52px]">
-          <button onClick={onClose} type="button" className="absolute right-[16px] top-[20px]">
+      <div className="flex-center fixed inset-0 h-svh w-full">
+        <div
+          className={`fixed inset-0 bg-gray-6 opacity-40 ${zIndex.backdrop}`}
+          ref={modalBackground}
+          onClick={handleBackgroundClick}
+          aria-hidden="true"
+        />
+        <div
+          className={`flex-column absolute w-[87.2%] max-w-[390px] rounded-xl bg-white pb-6 pt-[52px] ${zIndex.modal}`}
+        >
+          <button onClick={onClose} type="button" className="absolute right-4 top-5">
             <Icon name="close" />
           </button>
-          <div className="flex size-full flex-col items-center">{children}</div>
+          <div className="flex-column size-full items-center">{children}</div>
         </div>
       </div>
     )
