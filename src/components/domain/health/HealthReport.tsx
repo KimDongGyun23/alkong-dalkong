5ebd9bdfb1c025c62cf902ffe2dbf94bcf01bbcf
김동균 <wooheuy5752@nate.com>
2024-10-10 21:13:59 +0900

    feat : 체중이 음수일 경우, 부호에 따른 메세지 출력

--- src/components/domain/health/HealthReport.tsx
+++ src/components/domain/health/HealthReport.tsx
@@ -10,19 +10,26 @@ export const HealthReport = ({ report }: HealthReportProps) => {
   const { user } = useUserStore()
   const { apiAvgWeight, diffWeight, laskweekWeight } = report
 
+  const absDiffWeight = Math.abs(diffWeight)
+  const signDiffWeight = Math.sign(diffWeight)
+
+  const absLastWeekWeight = Math.abs(laskweekWeight)
+  const signLastWeekWeight = Math.sign(laskweekWeight)
+
   return (
     <>
       <p className="subtitle-M grow rounded-xl bg-mint-0 py-[14px] pl-6">
         {user.name}님과 같은 성별, 나이의 평균
         <br />
-        몸무게인 {apiAvgWeight}kg보다 {diffWeight}kg 낮아요.
+        몸무게인 {apiAvgWeight}kg보다 {absDiffWeight}kg{' '}
+        {signDiffWeight === 1 ? '높아요.' : '낮아요.'}
       </p>
 
       {laskweekWeight && (
         <p className="subtitle-M grow rounded-xl bg-mint-0 py-[14px] pl-6">
-          {user.name}님의 평균 체중이
+          {user.name}님의 체중이
           <br />
-          지난주보다 {laskweekWeight} 감소했어요.
+          지난주보다 {absLastWeekWeight} {signLastWeekWeight === 1 ? '증가했어요.' : '감소했어요.'}
         </p>
       )}
     </>
