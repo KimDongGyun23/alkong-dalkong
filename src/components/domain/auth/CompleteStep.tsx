8114d9cbb17aff23da6a61140d757476b5cfd662
김동균 <wooheuy5752@nate.com>
2024-10-29 23:09:00 +0900

    chore : 회원가입 마지막 페이지에서 타이머 작동 후, 자동 페이지 이동 (#4)

--- src/components/domain/auth/CompleteStep.tsx
+++ src/components/domain/auth/CompleteStep.tsx
@@ -1,16 +1,27 @@
 'use client'
+import { useEffect } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 import SignUpComplete from '/public/sign-up-complete.png'
 
 export const CompleteStep = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/sign-in')
+    }, 1500)
+
+    return () => clearTimeout(timer)
+  }, [router])
+
   return (
-    <Link href="/sign-in" replace className="flex-center min-h-screen w-full bg-mint-4">
+    <div className="flex-center min-h-screen w-full bg-mint-4">
       <div className="flex-column-align title-B gap-[12px] whitespace-pre text-center text-white">
         <Image src={SignUpComplete} alt="sign-up-complete" />
         {`회원가입 성공!\n로그인 창으로 넘어갈게요.`}
       </div>
-    </Link>
+    </div>
   )
 }
