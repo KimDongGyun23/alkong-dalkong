e6930d32ee932925e80d46468c31be6eccbac718
김동균 <wooheuy5752@nate.com>
2024-10-30 16:33:12 +0900

    feat : /에서 로그인 페이지로 이동 버튼 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div>
-      <p>HOME</p>
+    <div className="h-full p-5">
+      <Link className="block h-full rounded bg-mint-3 p-5" href={'/sign-in'}>
+        로그인 페이지 이동
+      </Link>
     </div>
   )
 }
